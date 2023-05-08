f17ed582e5cb6c8188ba0cad9c0849fd80ee03ae
Pablo <pablo.tyg@gmail.com>
2023-05-07 21:39:24 -0300

    TP integrador fase inicial v. 1.0

---
+++ priJS.js
@@ -0,0 +1,9 @@
+// formas de declarar variables 
+var nombre = "hola mundo";
+let numero = "codo a codo"
+// no se puede declarar variables antes de inicializarlas
+// const    .... Variable constante
+
+console.log(typeof nombre)
+console.log(numero)
+// alert("hola mundo");
